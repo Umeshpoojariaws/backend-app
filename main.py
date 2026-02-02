f9ad1cca59d9dba2e6fad4f7d2185417e37dcf68
Umesh Poojari <umeshpoojari@Umeshs-MacBook-Air.local>
2026-02-02 20:54:09 +0530

    updating model name

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ model = None
 @app.on_event("startup")
 def load_model():
     global model
-    model_name = "Weather-Forecast"
+    model_name = "weather-forecast-model"
     
     try:
         client = MlflowClient()
